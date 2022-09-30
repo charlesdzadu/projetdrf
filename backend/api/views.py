f303786cefc542cf69655d53439b7d203c827f0e
charlesdzadu <dkccharlie@gmail.com>
2022-09-30 22:40:10 +0000

    Begin serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,18 @@
-from django.http import JsonResponse, HttpRequest
-import json
+from email import header
+from django.http import JsonResponse, HttpRequest, HttpResponse
+from django.forms.models import model_to_dict
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+from products.models import Product
 
+@api_view(["GET"])
 def api_home( request: HttpRequest , *args, **kwargs):
-    body = request.body
-    data = {}
-    data = json.loads(body)
-    data['params'] = request.GET
-    data['content'] = request.content_type
-    data['headers'] = dict(request.headers)
-    return JsonResponse(data)
+    model_data = Product.objects.all().order_by("?").first()
+    
+    data = model_to_dict(model_data, exclude=['id'])
+    data['sale_price'] = model_data.sale_price
+    
+    return Response(data)
